a2cc9875e64bd9abf9c269e1736302b17ba9440e
kharuk <frends.nastya@gmail.com>
2019-04-15 12:34:24 +0300

    fix: add private root

--- routes/film.js
+++ routes/film.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const filmControllers = require('../controllers/filmControllers');
 //const auth = require('../middlewares/passportMiddleware');
+const passport =  require('passport');
 
 const router = express.Router();
-router.get('/films', filmControllers.getAllFilms);
+router.get('/films', passport.authenticate('jwt', { session: false }), filmControllers.getAllFilms);
 router.get('/films/:id', filmControllers.getFilm);
 router.post('/films', filmControllers.createFilm);
 router.put('/films/:id', filmControllers.updateFilm);
