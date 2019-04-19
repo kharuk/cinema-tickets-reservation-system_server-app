d9fb4e9ea50d3a73223ba393e105d088bc831f7f
kharuk <frends.nastya@gmail.com>
2019-04-19 22:45:03 +0300

    fix: change pagination

--- services/orderServices.js
+++ services/orderServices.js
@@ -3,28 +3,42 @@ const Order = require('../models/orderModel');
 
 
 function getOrders(req, res) {
-  const page = req.params.page ? req.params.page : 1;
+  const page = req.query.page ? +req.query.page : 1;
   const limit = 10;
-  console.log(req.params);
-  const dateQuerty = req.params.typeOfOrders === 'current' ? {$gte: new Date()} : {$lt: new Date()}
-  console.log('dateQuerty', dateQuerty);
-  Order.find({
+  const dateQuerty = req.query.typeOfOrders === 'current' ? {$gte: new Date()} : {$lt: new Date()}
+
+  Order.count({
     user_id: req.user._id,
     date: dateQuerty
-  })
-  .sort({date: 'desc'})
-  .limit(limit)
-  .skip(page*limit-limit)
-  .exec((err, result) => {
+  }, function(err, count) {
     if (!err) {
-      console.log(result);
-      res.json({orderList: result, isSuccessfully: true});
-      return;
+      Order.find({
+        user_id: req.user._id,
+        date: dateQuerty
+      })
+      .sort({date: 'desc'})
+      .limit(limit)
+      .skip(page*limit-limit)
+      .exec((err, result) => {
+        if (!err) {
+          res.json({
+            orderList: result, 
+            pageSize: limit,
+            totalAmount: count,
+            pageNumber: page,
+            isSuccessfully: true
+          });
+          return;
+        } else {
+          console.log(err);
+          res.status(500).send(err)
+        } 
+      });
     } else {
       console.log(err);
       res.status(500).send(err)
-    } 
-  })
+    }
+  });
 }
 
 function createOrder(req, res) {
